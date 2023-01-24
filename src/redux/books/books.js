dd3dc3cfcb2e81cff1b08d1e945a253a6c23788b
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-01-24 10:54:36 +0200

    Add few books to book reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,4 +1,5 @@
 import { createReducer } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
 
 const ADD = 'bookStore/books/ADD';
 const REMOVE = 'bookStore/books/REMOVE';
@@ -13,7 +14,30 @@ const removeBook = (id) => ({
   id,
 });
 
-const bookReducer = createReducer([], {
+const initialState = [
+  {
+    id: uuidv4(),
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+  },
+  {
+    id: uuidv4(),
+    title: 'Dune',
+    author: 'Frank Herbert',
+  },
+  {
+    id: uuidv4(),
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+  },
+  {
+    id: uuidv4(),
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+  },
+];
+
+const bookReducer = createReducer(initialState, {
   [ADD]: (state, action) => [...state, action.payload],
   [REMOVE]: (state, action) => state.filter((book) => book.id !== action.id),
 });
