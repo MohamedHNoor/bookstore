da8b8aa6a2c08b2aada1af47c63bd65450ca9bd1
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-01-26 13:07:37 +0200

    Modified src/component/Book.js

--- src/component/Book.js
+++ src/component/Book.js
@@ -11,13 +11,51 @@ const Book = ({ title, author, id }) => {
   };
 
   return (
-    <div className="book">
-      <h1>{title}</h1>
-      <p>{author}</p>
+    <div className="book-container">
+      <div className="book">
+        <div className="book-cover">
+          <h4 className="book-category">Fiction</h4>
+          <h2 className="book-title">{title}</h2>
+          <p className="book-author">{author}</p>
+        </div>
+        <div className="action-buttons">
+          <button type="button" className=" btn-primary">
+            Comments
+          </button>
+          <div className="divider" />
+          <button
+            type="button"
+            className="btn-primary"
+            onClick={() => handleRemoveBook()}
+          >
+            Remove
+          </button>
+          <div className="divider" />
+          <button type="button" className="btn-primary">
+            Edit
+          </button>
+        </div>
+      </div>
 
-      <button type="button" onClick={() => handleRemoveBook()}>
-        Remove
-      </button>
+      <div className="progress-container">
+        <div className="progress">
+          <div className="progress-circle-container">
+            <div className="circular-progress" />
+          </div>
+          <div className="progress-status">
+            <p className="progress-value">60%</p>
+            <p className="completed">Completed</p>
+          </div>
+        </div>
+        <div className="progress-divider" />
+        <div className="current-chapter">
+          <p className="current-chapter-title">CURRENT CHAPTER</p>
+          <p className="current-chapter-number">Chapter 17</p>
+          <button type="button" className="btn-update">
+            UPDATE PROGRESS
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
